0cd54b4f8603dce77e0b05789ed543aee71137b9
SmirnovAlex0891 <114992940+SmirnovAlex0891@users.noreply.github.com>
2022-10-14 23:10:39 +0200

    Add files via upload

---
+++ HW_12_10_2022/src/Exercise2.java
@@ -0,0 +1,13 @@
+public class Exercise2 {
+    public static void main(String[] args) {
+        //Напишите программу, которая выводит на экран сумму двух чисел.
+        // 54 + 16
+
+        byte sum = 54 + 16;
+
+        System.out.print ("Результат операции = ");
+        System.out.println(sum);
+
+
+    }
+}
---
+++ HW_12_10_2022/src/Exercise3.java
@@ -0,0 +1,13 @@
+public class Exercise3 {
+    public static void main(String[] args) {
+        //Напишите программу, которая выводит на экран результат деления двух чисел.
+        // 50/3
+
+        double doubleValue1 = 50;
+        double doubleValue2 = 3;
+        double doubleResultDiv = doubleValue1 / doubleValue2;
+
+        System.out.print ("Результат операции = ");
+        System.out.println(doubleResultDiv);
+    }
+}
